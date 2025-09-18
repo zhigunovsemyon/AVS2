1d101ef5480cde5618ba9cae53ff5bb234ff304b
Semyon <zhigunovsemen@gmail.com>
2025-09-18 10:38:42 +0300

    Добавил последовательное умножение в задании 1

--- src/main.c
+++ src/main.c
@@ -1,26 +1,50 @@
 #include <omp.h>
+#include <stdint.h>
 #include <stdio.h>
 
 static void task2()
 {
+	puts("\nЗадание 2");
 	double t = omp_get_wtime();
-	#pragma omp parallel
+#pragma omp parallel
 	{
-
 	}
 	t -= omp_get_wtime();
 	t *= -1;
 	printf("t = %lf\n", t);
 }
 
-static void task1()
+static double task1_serial(double a, double b, int64_t iter_count)
 {
+	double start_time = omp_get_wtime();
+	double foo;
+	for (int64_t i = 0; i < iter_count; ++i) {
+		foo = a * b;
+		(void)foo;
+	}
 
+	return omp_get_wtime() - start_time;
+}
+
+static void task1(int64_t iter_count)
+{
+	puts("Задание 1");
+	double a, b;
+	printf("Введите число a: ");
+	scanf("%lf", &a);
+	printf("Введите число b: ");
+	if (0 > scanf("%lf", &b)) {
+		return;
+	}
+	printf("a = %lf\nb = %lf\nчисло итераций = %ld\n", a, b, iter_count);
+
+	printf("Время последовательного умножения: %lfs\n",
+	       task1_serial(a, b, iter_count));
 }
 
 int main()
 {
-	task1();
+	task1(10 * (int64_t)UINT32_MAX);
 	task2();
 	return 0;
 }
